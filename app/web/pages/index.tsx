b81c22bf21f42268e1e175399930796a629c3825
anuwatavis <anuwat.avis@gmail.com>
2023-04-29 01:04:28 +0700

    feat(): add passport ui

--- app/web/pages/index.tsx
+++ app/web/pages/index.tsx
@@ -5,10 +5,18 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      Thailand National Park Passport
+    <main className="h-screen w-scree flex items-center">
+      <div className="mx-auto my-auto bg-nature-green p-4 rounded-md">
+        <div className="border border-white rounded-md p-1">
+          <div className="border border-white rounded-md p-4">
+            <div className="text-center text-gray-50">
+              <div className="text-md">หนังสือเดินทางท่องเที่ยว</div>
+              <div className="text-5xl my-3">อุทยานแห่งชาติ</div>
+              <div className="text-md">Passport to Thailand National Parks</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
